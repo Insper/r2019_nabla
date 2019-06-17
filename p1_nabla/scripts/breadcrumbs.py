2d7c3db1a37af3cb260d028c9ba82705fb22bdec
mirwox <fmiranda@gmail.com>
2019-06-17 12:54:12 -0300

    envio do breadcrumbs

---
+++ p1_nabla/scripts/breadcrumbs.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+import numpy as np
+import cv2
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Vector3
+import math
+
+
+def desenha(cv_image):
+    """
+        Use esta funcão como exemplo de como desenhar na tela
+    """
+    cv2.circle(cv_image,(256,256),64,(0,255,0),2)
+    cv2.line(cv_image,(256,256),(400,400),(255,0,0),5)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(cv_image,'Boa sorte!',(0,50), font, 2,(255,255,255),2,cv2.LINE_AA)
+
+if __name__=="__main__":
+
+    rospy.init_node("breadcrumbs")
+
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+
+
+    cv2.namedWindow("Saida")
+
+    branco_bgr = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
+    # Cria uma imagem 512 x 512
+    zero = (256, 256) # ponto considerado ponto zero
+
+    while not rospy.is_shutdown():
+        # Funcão que deve atualizar o desenho com as novas informacos de odometria
+        desenha(branco_bgr)
+        # Imprime a imagem de saida
+        cv2.imshow("Saida", branco_bgr)
+        cv2.waitKey(40) # Esperar 40 millisegundos
+        rospy.sleep(0.1)
+
+
+
