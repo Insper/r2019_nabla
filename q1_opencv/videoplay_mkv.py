6c7853d361c042d28314e86c1b4c0f8263c2501b
mirwox <fmiranda@gmail.com>
2019-06-17 13:47:14 -0300

    versao mkv

---
+++ q1_opencv/videoplay_mkv.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import cv2
+
+cap = cv2.VideoCapture('hall.mkv')
+
+# Para abrir a webcam use a opção abaixo
+# cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    print("Codigo de retorno", ret)
+
+    # Our operations on the frame come here
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
